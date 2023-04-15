f1f207e28165c067da604b0f07790e48fc291cc9
PiotrOlek <olo_piotr@onet.pl>
2023-04-15 13:07:54 +0200

    Inprogress

---
+++ Reservation of conference rooms App/script.js
@@ -0,0 +1,67 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const searchForm = document.querySelector("#search form");
+    const availableRooms = document.querySelector("#available-rooms");
+  
+    // Funkcja wyszukiwania dostępnych sal
+    const searchRooms = async (location, date, capacity) => {
+      try {
+        const response = await fetch(
+          `/api/rooms?location=${location}&date=${date}&capacity=${capacity}`
+        );
+        const rooms = await response.json();
+        displayAvailableRooms(rooms);
+      } catch (error) {
+        console.error("Error searching rooms:", error);
+      }
+    };
+  
+    // Funkcja wyświetlająca dostępne sale
+    const displayAvailableRooms = (rooms) => {
+      availableRooms.innerHTML = "";
+  
+      rooms.forEach((room) => {
+        const roomElement = document.createElement("div");
+        roomElement.classList.add("available-room");
+        roomElement.innerHTML = `
+          <h3>${room.name}</h3>
+          <p>Lokalizacja: ${room.location}</p>
+          <p>Pojemność: ${room.capacity}</p>
+          <p>Cena: ${room.price} zł</p>
+          <button data-room-id="${room.id}">Rezerwuj</button>
+        `;
+        availableRooms.appendChild(roomElement);
+      });
+  
+      availableRooms.querySelectorAll("button").forEach((button) => {
+        button.addEventListener("click", () => {
+          reserveRoom(button.dataset.roomId);
+        });
+      });
+    };
+  
+    // Funkcja rezerwacji sali
+    const reserveRoom = async (roomId) => {
+      try {
+        const response = await fetch(`/api/reserve/${roomId}`, {
+          method: "POST",
+        });
+        const reservation = await response.json();
+  
+        // Przekierowanie do strony płatności
+        window.location.href = `/payment?reservationId=${reservation.id}`;
+      } catch (error) {
+        console.error("Error reserving room:", error);
+      }
+    };
+  
+    searchForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const location = searchForm.location.value;
+      const date = searchForm.date.value;
+      const capacity = searchForm.capacity.value;
+  
+      searchRooms(location, date, capacity);
+    });
+  });
+  
