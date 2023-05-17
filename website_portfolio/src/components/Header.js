74689584b464f5405fe90142342e27fbf3caf7fe
piotr.olek <olo_piotr@onet.pl>
2023-05-17 20:10:04 +0200

    Update Header.js

--- website_portfolio/src/components/Header.js
+++ website_portfolio/src/components/Header.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
+import logo from '../images/logo.png';
 
 function Header() {
   return (
-    <header>
-      <h1>Moja Strona</h1>
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img src={logo} alt="Logo" />
+      </div>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link to="/">Strona główna</Link>
+          </li>
+          <li>
+            <Link to="/about">O mnie</Link>
+          </li>
+          <li>
+            <Link to="/education-experience">Edukacja i Doświadczenie</Link>
+          </li>
+          <li>
+            <Link to="/skills">Umiejętności</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projekty</Link>
+          </li>
+          <li>
+            <Link to="/contact">Kontakt</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
