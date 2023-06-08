596ec9a214b2f49a7960979af21c5cdb4fa9e171
PiotrOlek <128393116+PiotrOlek@users.noreply.github.com>
2023-06-08 19:29:59 +0200

    Done

--- generator-hasel/src/PasswordGenerator.jsx
+++ generator-hasel/src/PasswordGenerator.jsx
@@ -12,32 +12,37 @@ function PasswordGenerator() {
   const [useNumbers, setUseNumbers] = useState(true);
   const [useSpecialCharacters, setUseSpecialCharacters] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+  const [isOTPMode, setIsOTPMode] = useState(false);
   const [isGenerateButtonDisabled, setIsGenerateButtonDisabled] = useState(true);
 
+
   useEffect(() => {
-    setIsGenerateButtonDisabled(!useLowercase && !useUppercase && !useNumbers && !useSpecialCharacters);
+    setIsLoading(false);
+  }, [isOTPMode]);
+
+  useEffect(() => {
+    const isGenerateButtonDisabled = !useLowercase && !useUppercase && !useNumbers && !useSpecialCharacters;
+    setIsLoading(false);
+    setIsOTPMode(false);
+    setIsGenerateButtonDisabled(isGenerateButtonDisabled);
   }, [useLowercase, useUppercase, useNumbers, useSpecialCharacters]);
 
   function generatePassword() {
     setIsLoading(true);
-
     if (!useLowercase && !useUppercase && !useNumbers && !useSpecialCharacters) {
       alert('Musisz zaznaczyć przynajmniej jeden typ znaków!');
       setIsLoading(false);
       return;
     }
-
     let charset = '';
     if (useLowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
     if (useUppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     if (useNumbers) charset += '0123456789';
     if (useSpecialCharacters) charset += '!@#$%^&*()_+=-[]{};:,.<>?';
-
     let newPassword = '';
     for (let i = 0, n = charset.length; i < passwordLength; ++i) {
       newPassword += charset[Math.floor(Math.random() * n)];
     }
-
     setTimeout(() => {
       setPassword(newPassword);
       setPasswordStrength(evaluatePasswordStrength(newPassword));
@@ -57,15 +62,14 @@ function PasswordGenerator() {
 
   function generateOTP() {
     setIsLoading(true);
-
     let otp = '';
     for (let i = 0; i < 6; ++i) {
       otp += Math.floor(Math.random() * 10);
     }
-
     setTimeout(() => {
       setOtp(otp);
       setIsLoading(false);
+      setIsOTPMode(true);
     }, 500);
   }
 
@@ -78,72 +82,96 @@ function PasswordGenerator() {
   return (
     <div className="password-generator-container">
       <h2>Password Generator</h2>
-      <label>
-        Długość hasła:
-        <input
-          type="number"
-          min="1"
-          value={passwordLength}
-          onChange={(e) => setPasswordLength(Number(e.target.value))}
-        />
-      </label>
-      <div className="options-grid">
-        <label className="custom-checkbox">
-          <input
-            type="checkbox"
-            checked={useLowercase}
-            onChange={(e) => setUseLowercase(e.target.checked)}
-          />
-          <span className="custom-checkbox-label"></span>
-          Użyj małych liter
-        </label>
-        <label className="custom-checkbox">
-          <input
-            type="checkbox"
-            checked={useUppercase}
-            onChange={(e) => setUseUppercase(e.target.checked)}
-          />
-          <span className="custom-checkbox-label"></span>
-          Użyj dużych liter
-        </label>
-        <label className="custom-checkbox">
-          <input
-            type="checkbox"
-            checked={useNumbers}
-            onChange={(e) => setUseNumbers(e.target.checked)}
-          />
-          <span className="custom-checkbox-label"></span>
-          Użyj cyfr
-        </label>
-        <label className="custom-checkbox">
-          <input
-            type="checkbox"
-            checked={useSpecialCharacters}
-            onChange={(e) => setUseSpecialCharacters(e.target.checked)}
-          />
-          <span className="custom-checkbox-label"></span>
-          Użyj znaków specjalnych
-        </label>
-      </div>
-      <button className="generate-button" onClick={generatePassword} disabled={isGenerateButtonDisabled}>
-        Generuj hasło
-      </button>
-      <ClipLoader color={'#123abc'} loading={isLoading} size={50} />
-      <input type="text" id="password" value={password} readOnly />
-      <div className="password-strength-bar">
-        <div
-          className="password-strength-bar-fill"
-          style={{
-            width: `${passwordStrength * 20}%`
-          }}
-        />
-      </div>
-      <p className="password-strength-text">Siła hasła: {passwordStrength}/5</p>
-      <button className="copy-button" onClick={() => copyToClipboard(password)}>Kopiuj hasło</button>
-      <button className="otp-button" onClick={generateOTP}>Generuj OTP</button>
-      <ClipLoader color={'#123abc'} loading={isLoading} size={50} />
-      <input type="text" id="otp" value={otp} readOnly />
-      <button className="copy-button" onClick={() => copyToClipboard(otp)}>Kopiuj OTP</button>
+      {!isOTPMode && (
+        <>
+          <label>
+            Długość hasła:
+            <input
+              type="number"
+              min="1"
+              value={passwordLength}
+              onChange={(e) => setPasswordLength(Number(e.target.value))}
+            />
+          </label>
+          <div className="options-grid">
+            <label className="switch">
+              <input
+                type="checkbox"
+                checked={useLowercase}
+                onChange={(e) => setUseLowercase(e.target.checked)}
+              />
+              <span className="slider"></span>
+              Małe litery
+            </label>
+            <label className="switch">
+              <input
+                type="checkbox"
+                checked={useUppercase}
+                onChange={(e) => setUseUppercase(e.target.checked)}
+              />
+              <span className="slider"></span>
+              Wielkie litery
+            </label>
+            <label className="switch">
+              <input
+                type="checkbox"
+                checked={useNumbers}
+                onChange={(e) => setUseNumbers(e.target.checked)}
+              />
+              <span className="slider"></span>
+              Cyfry
+            </label>
+            <label className="switch">
+              <input
+                type="checkbox"
+                checked={useSpecialCharacters}
+                onChange={(e) => setUseSpecialCharacters(e.target.checked)}
+              />
+              <span className="slider"></span>
+              Znaki specjalne
+            </label>
+          </div>
+          {isLoading ? (
+            <div className="loading-container">
+              <ClipLoader color={'#123abc'} loading={true} size={50} />
+            </div>
+          ) : (
+            <>
+              <button className="generate-button" onClick={generatePassword} disabled={isGenerateButtonDisabled}>
+                Generuj hasło
+              </button>
+              <input type="text" id="password" value={password} readOnly />
+              <div className="password-strength-bar">
+                <div
+                  className="password-strength-bar-fill"
+                  style={{
+                    width: `${passwordStrength * 20}%`
+                  }}
+                />
+              </div>
+              <p className="password-strength-text">Siła hasła: {passwordStrength}/5</p>
+              <button className="copy-button" onClick={() => copyToClipboard(password)}>Kopiuj hasło</button>
+              <button className="otp-toggle-button" onClick={generateOTP}>Generuj OTP</button>
+            </>
+          )}
+        </>
+      )}
+      {isOTPMode && (
+        <div className="otp-container">
+          {isLoading ? (
+            <div className="loading-container">
+              <ClipLoader color={'#123abc'} loading={true} size={50} />
+            </div>
+          ) : (
+            <>
+              <input type="text" id="otp" value={otp} readOnly />
+              <p>OTP (One-Time Password) to jednorazowe hasło lub kod używane w celu uwierzytelnienia tożsamości użytkownika.</p>
+              <button className="copy-button" onClick={() => copyToClipboard(otp)}>Kopiuj OTP</button>
+              <button className="otp-toggle-button" onClick={() => setIsOTPMode(false)}>Generuj hasło</button>
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 }
