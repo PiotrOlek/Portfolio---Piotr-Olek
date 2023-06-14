d040d32ae335f1a4744b2f51f1e60746e7636580
piotr.olek <olo_piotr@onet.pl>
2023-06-14 21:36:54 +0200

    Done

--- gre-memory-game/src/components/cardsData.js
+++ gre-memory-game/src/components/cardsData.js
@@ -1,16 +1,16 @@
 const cardsData = [
-    { id: 1, image: '' },
-    { id: 2, image: 'link-do-obrazka-2' },
-    { id: 3, image: 'link-do-obrazka-3' },
-    { id: 4, image: 'link-do-obrazka-4' },
-    { id: 5, image: 'link-do-obrazka-5' },
-    { id: 6, image: 'link-do-obrazka-5' },
-    { id: 1, image: '' },
-    { id: 2, image: 'link-do-obrazka-2' },
-    { id: 3, image: 'link-do-obrazka-3' },
-    { id: 4, image: 'link-do-obrazka-4' },
-    { id: 5, image: 'link-do-obrazka-5' },
-    { id: 6, image: 'link-do-obrazka-5' },
+    { id: 1, image: 'https://icons.iconarchive.com/icons/paddy/web20rigami/256/skype-icon.png' },
+    { id: 2, image: 'https://martech.org/wp-content/uploads/2013/10/google-analytics-icon-250px.png' },
+    { id: 3, image: 'https://musiccenter.pl/wp-content/uploads/2019/07/reflex_250px.jpg' },
+    { id: 4, image: 'https://www.delianet.com/wp-content/uploads/2019/01/250px-Instagram_logo_2016.svg1_.png' },
+    { id: 5, image: 'https://thefamilycentre.org.au/wp-content/uploads/2017/07/services-icon-250px.png' },
+    { id: 6, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMf5Jp-SvPHyMZ73PXQClrMxQDuxHaDOaJnw&usqp=CAU' },
+    { id: 1, image: 'https://icons.iconarchive.com/icons/paddy/web20rigami/256/skype-icon.png' },
+    { id: 2, image: 'https://martech.org/wp-content/uploads/2013/10/google-analytics-icon-250px.png' },
+    { id: 3, image: 'https://musiccenter.pl/wp-content/uploads/2019/07/reflex_250px.jpg' },
+    { id: 4, image: 'https://www.delianet.com/wp-content/uploads/2019/01/250px-Instagram_logo_2016.svg1_.png' },
+    { id: 5, image: 'https://thefamilycentre.org.au/wp-content/uploads/2017/07/services-icon-250px.png' },
+    { id: 6, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMf5Jp-SvPHyMZ73PXQClrMxQDuxHaDOaJnw&usqp=CAU' },
   ];
   
   export default cardsData;
